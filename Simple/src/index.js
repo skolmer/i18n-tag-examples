7de60032d983304cd49878d8a3e3309882add087
Steffen Kolmer <steffen@kolmer.net>
2016-05-30 12:50:10 +0200

    Added language selector

--- Simple/src/index.js
+++ Simple/src/index.js
@@ -1,8 +1,25 @@
 import $ from 'jquery'
-import i18n from 'es2015-i18n-tag'
+import i18n, { i18nConfig } from 'es2015-i18n-tag'
 
 const user = 'Steffen'
 
 $(document).ready(() => {
-    $('#root').text(i18n`Hello ${user}`)
+    $('#localeSelector').change((event) => {
+        const locale = $(event.target).val();
+        if (locale !== 'en-US') {
+            $.getJSON(`translations/translation.${locale.substr(0, 2)}.json`, function (data) {
+                i18nConfig({ locales: locale, translations: data });
+                drawText();
+            });
+        } else {
+            i18nConfig({ locales: 'en-US', translations: [] });
+            drawText();
+        }
+    })
+
+    const drawText = function () {
+        $('#root').text(i18n`Hello ${user}`)
+    }
+
+    drawText();
 })
